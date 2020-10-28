e79d8c49c7b2f56517e5f68c135e191359c05420
Aleksei Moskvin <a.moskvin@propellerads.net>
2020-10-28 12:27:44 +0300

    do not use concurrent.future, parse line in main thread, use set multi

--- memc_load.py
+++ memc_load.py
@@ -6,9 +6,10 @@ import gzip
 import logging
 import os
 import sys
-from concurrent.futures import ThreadPoolExecutor, wait
+from multiprocessing import Process
 from optparse import OptionParser
 from queue import Queue
+from threading import Thread
 
 # pip install python-memcached
 import memcache
@@ -19,6 +20,8 @@ import memcache
 import appsinstalled_pb2
 
 NORMAL_ERR_RATE = 0.01
+MEMC_SOCKET_TIMEOUT = 10
+MEMC_BATCH_SIZE = 100
 AppsInstalled = collections.namedtuple("AppsInstalled", ["dev_type", "dev_id", "lat", "lon", "apps"])
 
 
@@ -30,28 +33,34 @@ def dot_rename(path):
 
 def insert_apps_installed(memc_addr, queue, errors, dry_run=False):
     ua = appsinstalled_pb2.UserApps()
-    memc_client = memcache.Client([memc_addr])
-    count = 0
+    memc_client = memcache.Client([memc_addr], socket_timeout=MEMC_SOCKET_TIMEOUT)
+
+    keys = dict()
     while True:
-        if count > 1000 and count % 1000 == 0:
-            logging.info(f'({memc_addr}) Insert {count} apps')
         apps_installed = queue.get()
         if not apps_installed:
+            if keys:
+                key_errors = memc_client.set_multi(keys)
+                if key_errors:
+                    logging.error(f'Get error when write to memcache {memc_addr}. Failed keys: {",".join(key_errors)}')
+                    errors.put(len(key_errors))
             return
         ua.lat = apps_installed.lat
         ua.lon = apps_installed.lon
         key = "%s:%s" % (apps_installed.dev_type, apps_installed.dev_id)
         ua.apps.extend(apps_installed.apps)
         packed = ua.SerializeToString()
-        try:
-            if dry_run:
-                logging.debug("%s - %s -> %s" % (memc_addr, key, str(ua).replace("\n", " ")))
-            else:
-                memc_client.set(key, packed)
-                count += 1
-        except Exception as exp:
-            logging.exception("Cannot write to memc %s: %s" % (memc_addr, exp))
-            errors.put(1)
+        if dry_run:
+            logging.debug("%s - %s -> %s" % (memc_addr, key, str(ua).replace("\n", " ")))
+            continue
+        if len(keys) < MEMC_BATCH_SIZE:
+            keys[key] = packed
+        else:
+            key_errors = memc_client.set_multi(keys)
+            keys.clear()
+            if key_errors:
+                logging.error(f'Get error when write to memcache {memc_addr}. Failed keys: {",".join(key_errors)}')
+                errors.put(len(key_errors))
 
 
 def parse_apps_installed(line):
@@ -87,60 +96,66 @@ def process_line(line, device_memc, errors):
     queue.put(apps_installed)
 
 
-def main(options):
+def process_file(fn, options):
+    logging_configure(options)
     devices = ['idfa', 'gaid', 'adid', 'dvid']
+    total = 0
+    errors_queue = Queue()
+    inserters = []
+
+    inserts_by_device_count = int(int(options.workers) / len(devices))
+    device_memc = dict()
+    for device in devices:
+        queue = Queue()
+        device_memc[device] = queue
+        for _ in range(inserts_by_device_count):
+            i = Thread(target=insert_apps_installed, args=(getattr(options, device), queue, errors_queue, options.dry))
+            i.start()
+            inserters.append(i)
+
+    logging.info('Processing %s' % fn)
+    logging.info(f'Threads for inserts app to memcache by devive: {inserts_by_device_count}')
+    fd = gzip.open(fn)
+
+    for line in fd:
+        line = line.strip()
+        if not line:
+            continue
+        total += 1
+        if total > 1000 and total % 1000 == 0:
+            logging.info(f'Read {total} lines from {fn}')
+        process_line(line, device_memc, errors_queue)
+
+    for queue in device_memc.values():
+        for _ in range(inserts_by_device_count):
+            queue.put(None)
+    logging.info(f'Wait while all apps from {fn} inserted')
+    for i in inserters:
+        i.join()
+
+    errors = 0
+    while not errors_queue.empty():
+        errors += errors_queue.get()
+
+    logging.info(f'Parsed {total} lines')
+    logging.info(f'Found {errors} errors')
+    err_rate = float(errors) / total
+    if err_rate < NORMAL_ERR_RATE:
+        logging.info("Acceptable error rate (%s). Successful load" % err_rate)
+    else:
+        logging.error("High error rate (%s > %s). Failed load" % (err_rate, NORMAL_ERR_RATE))
+    fd.close()
+    dot_rename(fn)
+
+
+def main(options):
+    file_handlers = list()
     for fn in glob.iglob(options.pattern):
-        total = 0
-        errors_queue = Queue()
-        inserters = []
-        pool = ThreadPoolExecutor(int(options.workers))
-        inserts_by_device_count = int(int(options.workers) / len(devices))
-        device_memc = dict()
-        for device in devices:
-            queue = Queue()
-            device_memc[device] = queue
-            for _ in range(inserts_by_device_count):
-                inserters.append(
-                    pool.submit(insert_apps_installed, getattr(options, device), queue, errors_queue, options.dry)
-                )
-        logging.info(f'Max threads for parse lines: {int(options.workers)}')
-        logging.info(f'Threads for inserts app to memcache: {int(options.workers)}')
-        logging.info(f'Threads for inserts app to memcache by devive: {inserts_by_device_count}')
-        logging.info('Processing %s' % fn)
-        fd = gzip.open(fn)
-        parsers = []
-
-        for line in fd:
-            line = line.strip()
-            if not line:
-                continue
-            total += 1
-            if total > 1000 and total % 1000 == 0:
-                logging.info(f'Read {total} lines from {fn}')
-            with ThreadPoolExecutor(max_workers=int(options.workers)) as executor:
-                parsers.append(executor.submit(process_line, line, device_memc, errors_queue))
-
-        logging.info(f'Wait while all lines from {fn} parsed')
-        wait(parsers)
-        for queue in device_memc.values():
-            for _ in range(inserts_by_device_count):
-                queue.put(None)
-        logging.info(f'Wait while all apps from {fn} inserted')
-        wait(inserters)
-
-        errors = 0
-        while not errors_queue.empty():
-            errors += errors_queue.get()
-
-        logging.info(f'Parsed {total} lines')
-        logging.info(f'Found {errors} errors')
-        err_rate = float(errors) / total
-        if err_rate < NORMAL_ERR_RATE:
-            logging.info("Acceptable error rate (%s). Successful load" % err_rate)
-        else:
-            logging.error("High error rate (%s > %s). Failed load" % (err_rate, NORMAL_ERR_RATE))
-        fd.close()
-        dot_rename(fn)
+        p = Process(target=process_file, args=(fn, options))
+        p.start()
+        file_handlers.append(p)
+    for p in file_handlers:
+        p.join()
 
 
 def proto_test():
@@ -159,11 +174,16 @@ def proto_test():
         assert ua == unpacked
 
 
+def logging_configure(opts):
+    logging.basicConfig(filename=opts.log, level=logging.INFO if not opts.dry else logging.DEBUG,
+                        format='[%(asctime)s] %(levelname).1s %(message)s', datefmt='%Y.%m.%d %H:%M:%S')
+
+
 if __name__ == '__main__':
     op = OptionParser()
     op.add_option("-t", "--test", action="store_true", default=False)
     op.add_option("-l", "--log", action="store", default=None)
-    op.add_option("-w", "--workers", action="store", default=10)
+    op.add_option("-w", "--workers", action="store", default=40)
     op.add_option("--dry", action="store_true", default=False)
     op.add_option("--pattern", action="store", default="/data/appsinstalled/*.tsv.gz")
     op.add_option("--idfa", action="store", default="127.0.0.1:33013")
@@ -171,8 +191,7 @@ if __name__ == '__main__':
     op.add_option("--adid", action="store", default="127.0.0.1:33015")
     op.add_option("--dvid", action="store", default="127.0.0.1:33016")
     (opts, args) = op.parse_args()
-    logging.basicConfig(filename=opts.log, level=logging.INFO if not opts.dry else logging.DEBUG,
-                        format='[%(asctime)s] %(levelname).1s %(message)s', datefmt='%Y.%m.%d %H:%M:%S')
+    logging_configure(opts)
     if opts.test:
         proto_test()
         sys.exit(0)
